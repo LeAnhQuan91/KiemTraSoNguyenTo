c258eeea01b920efff1305b972c811ae6e3f0dec
LeAnhQuan91 <165478725+LeAnhQuan91@users.noreply.github.com>
2024-05-10 12:00:19 +0700

    add

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Prime
+{
+
+class Program
+{
+static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Nhap so bat ky: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            if (number < 2)
+                Console.WriteLine(number + " khong phai la so nguyen to");
+            else
+            {
+                int i = 2;
+                bool check = true;
+                while (i <= Math.Sqrt(number))
+                {
+                    if (number % i == 0)
+                    {
+                        check = false;
+                        break;
+                    }
+                    i++;
+                }
+                if (check)
+                    Console.WriteLine(number + " la so nguyen to");
+                else
+                    Console.WriteLine(number + " khong phai la so nguyen");
+            }
+        }
+}
+}
